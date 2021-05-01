c7140105d4bdd5db984863982b51bcdf72ff2e50
jianyuanw <69199572+jianyuanw@users.noreply.github.com>
2021-05-01 23:50:36 +0800

    Added handler for sticker messages

--- bot.py
+++ bot.py
@@ -9,23 +9,39 @@ load_dotenv()
 TOKEN = os.environ.get('TOKEN')
 
 def verify_token() -> None:
-    logging.info('Verifying token')
+    logging.info('Verifying token...')
     bot = Bot(token=TOKEN)
     try:
         logging.info(f'Verified token | Bot details: {bot.get_me()}')
     except:
         logging.info('Failed to verify token')
 
+def start(update: Update, context: CallbackContext) -> None:
+    first_name = update.effective_user.first_name
+    logging.info(f'Received command /start from {first_name}')
+    update.message.reply_text(f'Lol {first_name}')
+
+def sticker(update: Update, context: CallbackContext) -> None:
+    first_name = update.effective_user.first_name
+    group_title = update.effective_chat.title
+    logging.info(f'Received sticker from {first_name} in {group_title}')
+    update.message.reply_text(f'Hi {first_name}, please engage in meaningful conversation.')
+
 def main() -> None:
     verify_token()
 
     updater = Updater(token=TOKEN)
-    # dispatcher = updater.dispatcher
+    dispatcher = updater.dispatcher
 
-    # dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(MessageHandler(Filters.sticker, sticker))
 
-    # updater.start_polling()
-    # updater.idle()
+    logging.info('Starting bot...')
+    updater.start_polling()
+    updater.idle()
 
 if __name__ == '__main__':
     main()
+
+# Ideas:
+# Catch message edits
