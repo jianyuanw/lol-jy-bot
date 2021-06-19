3fbf9bda09bf1177f5e2c4476b6bc61b0870961f
jianyuanw <69199572+jianyuanw@users.noreply.github.com>
2021-06-19 23:05:33 +0800

    Added countdown command and daily job

--- bot.py
+++ bot.py
@@ -1,16 +1,21 @@
+import datetime
 import logging
 import os
+import pytz
 import requests
 from dotenv import load_dotenv
 from random import randrange
 from telegram import Bot, Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext, Filters
+from telegram.ext.jobqueue import Days
 
-logging.basicConfig(filename='bot.log', format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
-# logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+# logging.basicConfig(filename='bot.log', format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 load_dotenv()
 TOKEN = os.environ.get('TOKEN')
+TEST_CHAT_ID = '-568078567'
+FUNNY_CHAT_ID = '-1001417249364'
 
 def verify_token() -> None:
     logging.info('Verifying token...')
@@ -54,6 +59,21 @@ def motivate(update: Update, context: CallbackContext) -> None:
     message = f'"{text}"\n- {author}'
     update.message.reply_text(message)
 
+def countdownto(update: Update, context: CallbackContext) -> None:
+    args = context.args
+    if len(args) == 0:
+        update.message.reply_text('Hi! Use "/countdownto DDMMM" to perform the countdown.')
+    else:
+        date_str = args[0]
+        try:
+            date_today = datetime.date.today()
+            date_str += str(date_today.year)
+            date_countdownto = datetime.datetime.strptime(date_str, '%d%b%Y').date()
+            days = (date_countdownto - date_today).days
+            update.message.reply_text(f'{days} more day(s) to freedom!')
+        except ValueError:
+            update.message.reply_text('Incorrect date format. Please enter in DDMMM format (eg. 01Jan).')
+
 def sticker(update: Update, context: CallbackContext) -> None:
     first_name = update.effective_user.first_name
     group_title = update.effective_chat.title
@@ -82,6 +102,18 @@ def all_messages(update: Update, context: CallbackContext) -> None:
     if 'hang' in message.lower():
         update.message.reply_text('Remember to include time/place/activity')
 
+def test(update: Update, context: CallbackContext) -> None:
+    date_countdownto = datetime.date(2021, 6, 24)
+    date_today = datetime.date.today()
+    days = (date_countdownto - date_today).days
+    context.bot.send_message(chat_id=FUNNY_CHAT_ID, text=f'{days} more day(s) to freedom!')
+
+def countdown_daily(context: CallbackContext) -> None:
+    date_countdownto = datetime.date(2021, 6, 24)
+    date_today = datetime.date.today()
+    days = (date_countdownto - date_today).days
+    context.bot.send_message(chat_id=FUNNY_CHAT_ID, text=f'Good morning. {days} more day(s) to freedom!')
+
 def main() -> None:
     verify_token()
 
@@ -93,10 +125,17 @@ def main() -> None:
     dispatcher.add_handler(CommandHandler('leave', leave))
     dispatcher.add_handler(CommandHandler('lol', lol))
     dispatcher.add_handler(CommandHandler('motivate', motivate))
+    dispatcher.add_handler(CommandHandler('countdownto', countdownto))
     # dispatcher.add_handler(MessageHandler(Filters.sticker, sticker))
     # dispatcher.add_handler(MessageHandler(Filters.document.gif, gif))
     # dispatcher.add_handler(MessageHandler(Filters.update.edited_message, edited_message))
     # dispatcher.add_handler(MessageHandler(Filters.text, all_messages))
+    dispatcher.add_handler(CommandHandler('test', test))
+
+    job_queue = updater.job_queue
+    timezone = pytz.timezone('Asia/Singapore')
+    time = datetime.time(hour=7, tzinfo=timezone)
+    job_queue.run_daily(countdown_daily, time)
 
     logging.info('Starting bot...')
     updater.start_polling()
